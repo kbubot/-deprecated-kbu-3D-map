7349890a563fc76efcafd90aff7658f0a86e55f6
Brave Kim <ygflove@likelion.org>
2021-08-03 13:46:11 +0900

    :adhesive_bandage: do antialias when set render

--- src/World/systems/renderer.js
+++ src/World/systems/renderer.js
@@ -2,7 +2,10 @@ import { WebGLRenderer } from "three";
 import { sizes } from "../utils";
 
 function createRenderer(canvas) {
-  const renderer = new WebGLRenderer({ canvas: canvas });
+  const renderer = new WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+  });
   renderer.physicallyCorrectLights = true;
   renderer.setSize(sizes.width, sizes.height)
   renderer.shadowMap.enabled = true;
